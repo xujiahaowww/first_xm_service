4cc778d46b6df62c4f0447dc06503d14258948a1
xujiahao <497876383@qq.com>
2024-04-24 16:49:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let express = require('express') // 引入express
 let app = express() // 相当于 http.createServer(app)
+const cors = require('cors')
+let bodyParser = require('body-parser')
 //----- 配置跨域 -----
 
+app.use(cors())
 // express框架解决跨域问题的代码，注意该代码要放在 app.use(router); 之前
 app.all("*", (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -10,7 +13,9 @@ app.all("*", (req, res, next) => {
     res.header("X-Powered-By", "Express");
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
-  });
+});
+
+app.use(bodyParser.json());
 
 app.use('/api', require('./config/api'))
 // 监听3000端口
